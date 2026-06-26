97caf579f246abf817250efb7b91fe7b8ab611b1
defaultuser <default@example.com>
2026-06-26 15:13:29 +0800

    ~

--- cp_t1/t1.py
+++ cp_t1/t1.py
@@ -1,16 +1,18 @@
 import os
+os.environ["CUBLASLT_WORKSPACE_SIZE"] = "0"
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":0:0"
+
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as checkpoint
-os.environ["CUBLASLT_WORKSPACE_SIZE"] = "0"
-os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 
 torch.cuda.memory._record_memory_history()
 model = nn.Sequential(
     nn.Linear(100, 4096, device='cuda'),
     nn.Linear(4096, 256, device='cuda'),
     nn.Linear(256, 10, device='cuda')
-).to('cuda')
+)
+
 # Dummy data
 batch_size = 32
 x = torch.randn(batch_size, 100, device='cuda')
@@ -21,7 +23,7 @@ y_prime = checkpoint.checkpoint(
     use_reentrant=False
 )
 
-loss=y_prime.sum()
+loss_but_in_vec = torch.ones_like(y_prime)
+y_prime.backward(loss_but_in_vec)
 
-loss.backward()
 torch.cuda.memory._dump_snapshot('abc.pickle')
