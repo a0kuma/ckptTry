58bec9ecec7ab41f7ae62a22fdbabf81f4dccdef
defaultuser <default@example.com>
2026-06-26 11:36:59 +0800

    .

--- cp_t1/.ipynb_checkpoints/t1-checkpoint.py
+++ cp_t1/.ipynb_checkpoints/t1-checkpoint.py
@@ -1,24 +1,30 @@
+import os
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as checkpoint
+os.environ["CUBLASLT_WORKSPACE_SIZE"] = "0"
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
+
 torch.cuda.memory._record_memory_history()
 model = nn.Sequential(
     nn.Linear(100, 4096, device='cuda'),
-    nn.Linear(4096, 4096, device='cuda'),
-    nn.Linear(4096, 10, device='cuda')
+    nn.Linear(4096, 128, device='cuda'),
+    nn.Linear(128, 10, device='cuda')
 ).to('cuda')
 # Dummy data
 batch_size = 32
 x = torch.randn(batch_size, 100, device='cuda')
-y = torch.randint(0, 10, (batch_size,), device='cuda')
-y_prime = checkpoint.checkpoint_sequential(
+
+y_prime = checkpoint.checkpoint(#_sequential(
     model,
-    segments=1,
+    #segments=1,
     input=x,
     use_reentrant=False
 )
-crossEntropyLoss_func_obj=nn.CrossEntropyLoss()
-loss=crossEntropyLoss_func_obj(y_prime,y)
+#crossEntropyLoss_func_obj=nn.CrossEntropyLoss()
+#y = torch.randint(0, 10, (batch_size,), device='cuda')
+
+loss=y_prime.sum()#crossEntropyLoss_func_obj(y_prime,y)
 #dummy_input = torch.randn(4800, 100000, device='cuda')
 loss.backward()
 #del dummy_input
