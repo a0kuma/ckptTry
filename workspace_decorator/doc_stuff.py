9bb5b66a114b04065b6af2570c157c39c64337cd
defaultuser <default@example.com>
2026-07-10 16:54:18 +0800

    that is cool

---
+++ workspace_decorator/doc_stuff.py
@@ -0,0 +1,10 @@
+from icecream import ic
+def a():
+  """
+  stuff
+  """
+  return 3
+ic(a())
+ic(a.__doc__)
+#help(a)
+ic(a.__name__)
--- workspace_decorator/index.py
+++ workspace_decorator/index.py
@@ -1,13 +1,10 @@
 from icecream import ic
-#def a(b):
-#  ic(b)
-#@a
-#def c():
-#  ic(2)
-#c()
 def a(b):
-  ic(self)
-  c=52
-  d=b+1
-  return d+1
-ic(a(3))
+  ic(b)
+  def y():
+    ic(3)
+  return y
+@a
+def c():
+  ic(2)
+ic(c())
