73b764faebd70c0bbe7b857cac11fdab8e022e40
defaultuser <default@example.com>
2026-07-22 20:35:46 +0800

    ?

--- workspace_pack_hook/index.py
+++ workspace_pack_hook/index.py
@@ -17,18 +17,22 @@ model = nn.Sequential(
 x = torch.randn(1, 2,  device='cuda')
 
 def pack_hook(i):
-  return i
-  #if any( i.untyped_storage().data_ptr() == tmp_p.untyped_storage().data_ptr() for tmp_l in model for tmp_p in tmp_l.parameters()):
-    #return i
-  #else:
-    #return i
+  #return i
+  if any( i.untyped_storage().data_ptr() == tmp_p.untyped_storage().data_ptr() for tmp_l in model for tmp_p in tmp_l.parameters()):
+    ic(i)
+    return torch.ones(3,6,device='cuda')
+  else:
+    ic(i)
+    return i
 
 def unpack_hook(i):
-  return i
-  #if hasattr( i, 'untyped_storage' ) and any( i.untyped_storage().data_ptr() == tmp_p.untyped_storage().data_ptr() for tmp_l in model for tmp_p in tmp_l.parameters()):
-    #return i
-  #else:
-    #return i
+  #return i
+  if hasattr( i, 'untyped_storage' ) and any( i.untyped_storage().data_ptr() == tmp_p.untyped_storage().data_ptr() for tmp_l in model for tmp_p in tmp_l.parameters()):
+    ic(i)
+    return i
+  else:
+    ic(i)
+    return i
 
 with torch.autograd.graph.saved_tensors_hooks(
   pack_hook,
